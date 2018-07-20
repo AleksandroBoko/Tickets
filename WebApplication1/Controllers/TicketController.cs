a86e389ddf2381fc1acd2d71cfd64a5650e45056
Nikita Nemtsov <n.nemtsov@andersenlab.com>
2018-07-20 10:50:39 +0300

    Added controller, model, view Ticket

---
+++ WebApplication1/Controllers/TicketController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    public class TicketController : Controller
+    {
+        private readonly IList<Ticket> tickets;
+
+        public TicketController()
+        {
+            tickets = new List<Ticket>()
+            {
+                new Ticket(){Id = 1, Name = ".Net meet up", Date = DateTime.Now, Cost = 40.5f, Description = ".Net"},
+                new Ticket(){Id = 2, Name = "JS meet up", Date = DateTime.Now, Cost = 30.5f, Description = ".Net"},
+                new Ticket(){Id = 3, Name = "ReactJs meet up", Date = DateTime.Now, Cost = 10.4f, Description = ".Net"},
+            };
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(tickets);
+        }
+    }
+}
---
+++ WebApplication1/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public float Cost { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
